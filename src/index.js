18ca6e4a874842174cea0e0e0d2cfc12bb4251d8
daniel-cohyk <daniel.chettiar@cohyk.com>
2022-09-09 12:13:06 +0530

    Working on pushing to repo and pkg

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Button from './components/styled/button/button';
+export { Button }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
